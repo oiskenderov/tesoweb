1d735dd20138c8d8acfd147ce9893077d8c8a04d
oiskenderov <oiskenderov@gmail.com>
2024-05-18 14:30:07 +0400

    add terraform instruction

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "my_bucket_region" {
+    description = "my default bucket region"
+    type = string
+    default = "us-west-1"
+}
+
+variable "my_bucket_name" {
+    description = "my bucket name"
+    type = string
+    default = "orkhanapp2"
+}
+
