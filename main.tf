c70cf03df20141b7e221be0c4f78a4765cf0259c
oiskenderov <oiskenderov@gmail.com>
2024-05-30 01:14:49 +0400

    fix: main and variable

--- main.tf
+++ main.tf
@@ -83,3 +83,73 @@ resource "aws_s3_object" "Bucket_files" {
   # ETag of the S3 object
   etag = each.value.digests.md5
 }
+
+
+locals {
+  s3_bucket_name = var.my_bucket_name
+  domain         = "test.teso.az"
+  hosted_zone_id = var.hosted_zone_var
+  cert_arn       = var.cert_arn_id
+}
+
+resource "aws_cloudfront_distribution" "demo-bucket" {
+  enabled = true
+  aliases = [local.domain]
+  default_root_object = "index.html"
+  is_ipv6_enabled = true
+  wait_for_deployment = true
+
+  
+  default_cache_behavior {
+    allowed_methods = ["GET", "HEAD", "OPTIONS"]
+    cached_methods = ["GET", "HEAD", "OPTIONS"]
+    cache_policy_id = "658327ea-f89d-4fab-a63d-7e88639e58f6"
+    target_origin_id = aws_s3_bucket.demo-bucket.id
+    viewer_protocol_policy = "redirect-to-https"
+
+  }
+
+
+  origin {
+    domain_name = aws_s3_bucket.demo-bucket.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.demo-bucket.id
+    origin_id   = aws_s3_bucket.demo-bucket.bucket
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = local.cert_arn
+    minimum_protocol_version = "TLSv1.2_2021"
+    ssl_support_method = "sni-only"   
+    cloudfront_default_certificate = true
+  }
+}
+
+resource "aws_cloudfront_origin_access_control" "demo-bucket" {
+  name = "s3-cloudfront-test"
+  origin_access_control_origin_type = "s3"
+  signing_behavior = "always"
+  signing_protocol = "sigv4"
+}
+
+data "aws_iam_policy_document" "aws_cloudfront_oac_access" {
+  statement {
+    principals {
+      identifiers = ["cloudfront.amazonaws.com"]
+      type = "Service"
+    }
+
+    actions = ["s3:GetObjects"]
+    resources = ["${aws_s3_bucket.demo-bucket.arn}/*"]
+  }
+    
+}
+resource "aws_cloudfront_origin_access_identity" "demo-bucket" {
+  comment = "Access Identity for S3 bucket"
+}
+
--- variables.tf
+++ variables.tf
@@ -7,6 +7,17 @@ variable "my_bucket_region" {
 variable "my_bucket_name" {
     description = "my bucket name"
     type = string
-    default = "orkhanapp2"
+    default = "orkhanapp"
 }
 
+variable "cert_arn_id" {
+    description = "my east certficate"
+    type = string
+    default = "arn:aws:acm:us-east-1:218220018863:certificate/43a169c9-a116-4952-827f-4d7acb4a9e0c"
+}
+
+variable "hosted_zone_var" {
+    description = "my hosted zone"
+    type = string
+    default = "Z09831852FL88FQ1R79QL"
+}
